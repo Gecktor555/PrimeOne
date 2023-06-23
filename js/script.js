f5a37bb9c94f5e599a486e0a3bf92e6c34514549
Viktor <121763231+ViktorMalkin@users.noreply.github.com>
2023-06-23 21:31:48 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -52,3 +52,32 @@ if (iconMenu) {
 		menuHeader.classList.toggle('_active');
 	});
 }
+
+const menuLinks = document.querySelectorAll('.header-bottom-menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLimk => {
+		menuLimk.addEventListener('click', onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header-bottom').offsetHeight;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock')
+				iconMenu.classList.remove('_active');
+				//menuBody.classList.remove('_active');
+				//headerBottomMenu.classList.remove('_active');
+				menuHeader.classList.remove('_active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
