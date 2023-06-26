9b262d6c0ccc081ae12d70b166f2aee6d84c98cc
Viktor <121763231+ViktorMalkin@users.noreply.github.com>
2023-06-26 08:57:45 +0200

    Create lang.js

---
+++ js/lang.js
@@ -0,0 +1,42 @@
+const data = {
+	"english":
+	{
+		"toplink": "Online ordering",
+		"btnlink0": "ABOUT",
+		"btnlink1": "MENUS",
+		"btnlink2": "Service Info",
+		"btnlink3": "Gallery",
+		"btnlink4": "Friends",
+		"btnlink5": "Contact"
+	},
+	"spanish":
+	{
+		"toplink": "Pedidos en línea",
+		"btnlink0": "ACERCA DE",
+		"btnlink1": "MENÚS",
+		"btnlink2": "Información de servicio",
+		"btnlink3": "Galería",
+		"btnlink4": "Friends",
+		"btnlink5": "Contacto"
+	},
+	"ukrainian":
+	{
+		"toplink": "Онлайн замовлення",
+		"btnlink0": "ПРО НАС",
+		"btnlink1": "МЕНЮ",
+		"btnlink2": "Сервісна інформація",
+		"btnlink3": "Галерея",
+		"btnlink4": "Друзі",
+		"btnlink5": "Контакт"
+	},
+	"rusian":
+	{
+		"toplink": "Онлайн-заказ",
+		"btnlink0": "О НАС",
+		"btnlink1": "МЕНЮ",
+		"btnlink2": "Сервисная информация",
+		"btnlink3": "Галерея",
+		"btnlink4": "Друзья",
+		"btnlink5": "Контакт"
+	},
+}
